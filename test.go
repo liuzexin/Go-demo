41cbfb5995f40ea9a8c265950f56597e5178e44b
liuzexin <10848224@qq.com>
2017-07-24 20:47:22 +0800

    modified:   test.go

--- test.go
+++ test.go
@@ -1,2 +1,20 @@
+package main
+import "fmt"
 
-ist4easa
+type MyStruct struct{
+    x int
+    y int
+}
+
+func main() {
+    var a []int
+    var b = MyStruct{1,2}
+    if b.x == 1{
+        fmt.Println(b.x)
+    }
+    fmt.Printf("hello,word \n", a, b.x)
+}
+func anthoer(){
+    var b = make([]int,3, 5)
+    fmt.Printf("test arr\n", cap(b))
+}
