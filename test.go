27ef6a600385f8b6c946527eb1796ec90050c277
liuzexin <10848224@qq.com>
2017-07-26 09:54:08 +0800

    modified:   test.go

--- test.go
+++ test.go
@@ -5,16 +5,45 @@ type MyStruct struct{
     x int
     y int
 }
-
+//test
+/**
+var m = map[string]Vertex{
+    "Bell Labs": Vertex{
+        40.68433, -74.39967,
+    },
+    "Google": Vertex{
+        37.42202, -122.08408,
+    },
+}
+*/
 func main() {
     var a []int
     var b = MyStruct{1,2}
     if b.x == 1{
         fmt.Println(b.x)
     }
+    other()
+    second()
     fmt.Printf("hello,word \n", a, b.x)
 }
-func anthoer(){
+func another(){
     var b = make([]int,3, 5)
     fmt.Printf("test arr\n", cap(b))
+}
+
+var m map[string]MyStruct
+
+func other(){
+    m = make(map[string]MyStruct)
+    m["test"] = MyStruct{
+        10,
+        20,
+    }
+    fmt.Println(m["test"])
+}
+func second(){
+    testMap := make([]int, 10)//创建map需要使用此函数
+    for i := range testMap{
+        fmt.Printf("%d,%d \n",i,  testMap[i])
+    }
 }
