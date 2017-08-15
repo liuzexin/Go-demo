568ff30d5da32be9f296a0a8c9d179debec5db4b
liuzexin <10848224@qq.com>
2017-08-15 09:52:52 +0800

    modified:   function 	modified:   function.go

--- function.go
+++ function.go
@@ -4,9 +4,19 @@ import "fmt"
 func main(){
     res := test(1, 2)
     fmt.Println(res)
+    var arr []int
+    arr=[]int{1, 3, 4}
+    argTest(arr...)
+    argTest(1, 2, 3)
 }
 
 func test(y int, x int)( z int){
     z = x + y
     return // directly return with no variable
+}
+
+func argTest(arr ...int){
+    for _,val := range arr{
+        fmt.Println(val)    
+    }
 }
