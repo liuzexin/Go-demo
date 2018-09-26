d6b1fd449543f796cdce12496eeb8cdb0bb08220
liuzexin <10848224@qq.com>
2018-09-26 17:33:07 +0800

    new code

--- checknil.go
+++ checknil.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 type MyStruct struct {
 	a int
+	b int
 }
 
 func main() {
@@ -18,4 +22,31 @@ func main() {
 	if test == nil {
 		fmt.Println("true")
 	}
+
+	var c int
+	if &c == nil {
+		fmt.Println("true")
+	}
+	var s []int
+
+	if s == nil {
+		fmt.Println("--true")
+	}
+
+	val2 := MyStruct{}
+	val3 := &val2
+	if &val2 == val {
+		fmt.Println("---------true")
+	}
+	if &val2 == val3 {
+		fmt.Println("pointer--------true")
+	}
+
+	if reflect.DeepEqual(val, val2) {
+		fmt.Println("true")
+	}
+	if val2 == *val {
+		fmt.Println("----true")
+	}
+
 }
